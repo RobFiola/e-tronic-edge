ed1e5402e4377380771d89ea1eac93c1077725f6
RobFiola <38474642+RobFiola@users.noreply.github.com>
2018-04-17 14:06:07 -0500

    Initial Commit

---
+++ Overlay.js
@@ -0,0 +1,29 @@
+var openned=false;
+$(document).ready(function(){
+    $('.buttons').hover(function(){ 
+      if(!openned){
+        $('.opinion').addClass('open');
+      	openned=true;
+      }
+    });
+    $("input[name='rating']").click(function(){
+//          var thisid=this.value;
+//       alert(this.value);
+//       var thisnum=thisid.substr(thisid.length-2,thisid.length-1);
+      if(this.value<4){
+
+        $('.feedback').addClass('open');
+      }
+          //  $('.opinion').removeClass('open');
+    });
+  $('#close').click(function(){
+   $('.opinion').removeClass('open'); 
+    $('.feedback').removeClass('open');
+  });
+  
+  $('#feedback-btn').click(function(e){
+   e.preventDefault(); 
+    $('.opinion').removeClass('open'); 
+    $('.feedback').removeClass('open'); 
+  });
+});
